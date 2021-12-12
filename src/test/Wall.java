e85fa99456d13e7a43e0df1dc5e22c45aa0f2861
DBIG8 <83467264+DBIG8@users.noreply.github.com>
2021-12-13 03:41:44 +0800

    playerScore is added

--- src/test/Wall.java
+++ src/test/Wall.java
@@ -208,17 +208,21 @@ public class Wall {
                 //Vertical Impact
                 case Brick.UP_IMPACT:
                     ball.reverseY();
+                    playerScore += 100;
                     return b.setImpact(ball.down, Brick.Crack.UP);
                 case Brick.DOWN_IMPACT:
                     ball.reverseY();
+                    playerScore += 100;
                     return b.setImpact(ball.up,Brick.Crack.DOWN);
 
                 //Horizontal Impact
                 case Brick.LEFT_IMPACT:
                     ball.reverseX();
+                    playerScore += 100;
                     return b.setImpact(ball.right,Brick.Crack.RIGHT);
                 case Brick.RIGHT_IMPACT:
                     ball.reverseX();
+                    playerScore += 100;
                     return b.setImpact(ball.left,Brick.Crack.LEFT);
             }
         }
