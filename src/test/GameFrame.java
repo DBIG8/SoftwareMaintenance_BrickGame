7ab5c7280e1ebc40cae4b77848540abfa4590a88
DBIG8 <83467264+DBIG8@users.noreply.github.com>
2021-12-13 03:39:03 +0800

    Added enable and disable Instruction menu function

--- src/test/GameFrame.java
+++ src/test/GameFrame.java
@@ -1,39 +1,43 @@
-/*
- *  Brick Destroy - A simple Arcade video game
- *   Copyright (C) 2017  Filippo Ranza
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package test;
+package main.java;
 
+import gameboard.GameBoard;
+import highscore.HighScore;
+import homemenu.HomeMenu;
+import instruction.Instruction;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
-import java.awt.event.WindowListener;
 
 
+/**
+ * GameFrame Class
+ */
 public class GameFrame extends JFrame implements WindowFocusListener {
 
     private static final String DEF_TITLE = "Brick Destroy";
 
-    private GameBoard gameBoard;
+    public GameBoard gameBoard;
     private HomeMenu homeMenu;
+    private Instruction instruction;
+    private HighScore highScore;
 
     private boolean gaming;
 
+    private int score;
+    private Integer lastPlayerScore;
+    private Integer[] highscoreScore;
+    private ArrayList<Integer> highScoreList;
+
+    /**
+     * generate game frame
+     */
     public GameFrame(){
         super();
 
@@ -45,13 +49,18 @@ public class GameFrame extends JFrame implements WindowFocusListener {
 
         homeMenu = new HomeMenu(this,new Dimension(450,300));
 
+        instruction = new Instruction(this,new Dimension(450,300));
+
+        highScore = new HighScore(this,new Dimension(450,300));
+
         this.add(homeMenu,BorderLayout.CENTER);
 
         this.setUndecorated(true);
-
-
     }
 
+    /**
+     * Initializes GameFrame
+     */
     public void initialize(){
         this.setTitle(DEF_TITLE);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -60,6 +69,9 @@ public class GameFrame extends JFrame implements WindowFocusListener {
         this.setVisible(true);
     }
 
+    /**
+     * enable game board screen
+     */
     public void enableGameBoard(){
         this.dispose();
         this.remove(homeMenu);
@@ -68,9 +80,137 @@ public class GameFrame extends JFrame implements WindowFocusListener {
         initialize();
         /*to avoid problems with graphics focus controller is added here*/
         this.addWindowFocusListener(this);
-
     }
 
+    /**
+     * disable game board screen
+     */
+    public void disableGameBoard(){
+        this.dispose();
+        this.remove(gameBoard);
+        this.add(homeMenu,BorderLayout.CENTER);
+        this.setUndecorated(true);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+    /**
+     * enable Instruction screen
+     */
+    public void enableInstruction(){
+        this.dispose();
+        this.remove(homeMenu);
+        this.add(instruction,BorderLayout.CENTER);//here boom
+        this.setUndecorated(true);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+    /**
+     * disable Instruction screen
+     */
+    public void disableInstruction(){
+        this.dispose();
+        this.remove(instruction);
+        this.add(homeMenu,BorderLayout.CENTER);
+        this.setUndecorated(true);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+
+
+    /**
+     * enableHighscore screen
+     */
+    //public void enableHighScore(){
+        ReadFile();
+        highscoreScore = new Integer[highScoreList.size()];
+        highScoreList.toArray (highscoreScore);
+        highScore.setSCORE(highscoreScore,lastPlayerScore);
+        this.dispose();
+        this.remove(homeMenu);
+        this.add(highScore,BorderLayout.CENTER);//here boom
+        this.setUndecorated(true);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+    /**
+     * read the high score name and high score text file
+     */
+    private void ReadFile() {
+        try {
+
+            highScoreList= new ArrayList<Integer>();
+            File leaderboardScore = new File("C:\\Users\\Lenovo\\IdeaProjects\\Brick_Destroy-master\\resources\\scoreList.txt");
+            Scanner scanner1 = new Scanner(leaderboardScore);
+            int x =0;
+            while (scanner1.hasNext()) {
+                if(scanner1.hasNextInt()){
+                    highScoreList.add(scanner1.nextInt());
+                }
+
+            }
+            scanner1.close();
+
+            lastPlayerScore = highScoreList.get(highScoreList.size()-1);
+            Collections.sort(highScoreList, Collections.reverseOrder());
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("txt files failed to open.");
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * update high score name and high score list txt file
+     * @param score high score array
+     */
+    public static void WriteFile(int score){
+        try {
+            System.out.println("Save Succeeded");
+            FileWriter highscoreScore = new FileWriter("C:\\Users\\Lenovo\\IdeaProjects\\Brick_Destroy-master\\resources\\scoreList.txt",true);
+            highscoreScore.write(String.format("\r\n%d", score));
+            highscoreScore.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * disable high score screen
+     */
+   // public void disableHighScore(){
+
+        this.dispose();
+        this.remove(highScore);
+        this.add(homeMenu,BorderLayout.CENTER);
+        this.setUndecorated(true);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public int getScore(){
+        return score;
+    }
+
+    /**
+     * set the position of the screen
+     */
     private void autoLocate(){
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (size.width - this.getWidth()) / 2;
@@ -78,7 +218,10 @@ public class GameFrame extends JFrame implements WindowFocusListener {
         this.setLocation(x,y);
     }
 
-
+    /**
+     * allow user to play the game when the window screen is focused
+     * @param windowEvent indicate user's window screen
+     */
     @Override
     public void windowGainedFocus(WindowEvent windowEvent) {
         /*
@@ -92,6 +235,10 @@ public class GameFrame extends JFrame implements WindowFocusListener {
         gaming = true;
     }
 
+    /**
+     * everything stops when window screen lost focused
+     * @param windowEvent indicate user's window screen
+     */
     @Override
     public void windowLostFocus(WindowEvent windowEvent) {
         if(gaming)
